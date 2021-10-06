05ecc6f073102067bab30061035308ce83445ba9
Masatoshi Urakawa <bokunolog@gmail.com>
2021-10-06 10:15:24 +0900

    ブログ一覧のページ　修正 *リロード時、すべて表示 *BOXの左寄せ

--- pages/page-works.php
+++ pages/page-works.php
@@ -27,12 +27,43 @@
 		<div class="container">
 			<div class="postNav">
 				<ul class="postNavList">
-					<li class="js-tab-list is-select">ロードサイン</li>
+					<li class="js-tab-list is-select" style="display: none;"></li>
+					<li class="js-tab-list">ロードサイン</li>
 					<li class="js-tab-list">一般製作</li>
 					<li class="js-tab-list">その他</li>
 				</ul>
 			</div>
 			<div class="js-tab-contet">
+				<ul class="worksPost">
+					<?php
+					    $paged = get_query_var('paged', 1);
+					    $args = array(
+					        'paged' => $paged,
+					        'post_type' => 'post',
+					        'posts_per_page' => 12,
+					    );
+					    $query = new WP_Query($args);
+					?>
+					<?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+						<li>
+							<a href="<?php the_permalink(); ?>">
+								<dl class="worksPostBox">
+									<dt><?php the_post_thumbnail(''); ?></dt>
+									<dd>
+										<p><?php the_time("Y.m.d"); ?></p>
+										<h2><?php the_title(); ?></h2>
+									</dd>
+								</dl>
+							</a>
+						</li>
+					<?php endwhile;endif; ?>
+				</ul>
+				<div class="worksPostNav">
+					<?php wp_pagenavi(array('query' => $query)); ?>
+				</div>
+				<?php wp_reset_postdata(); ?>
+			</div>
+			<div class="js-tab-contet is-hide">
 				<ul class="worksPost">
 					<?php
 					    $paged = get_query_var('paged', 1);
@@ -127,42 +158,6 @@
 			</div>				
 		</div>
 	</section>
-
-	<?php /*
-	<section class="section">
-		<div class="worksBox">
-			<div class="worksBox_img">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/works/thumbnail_1.jpg" alt="">
-			</div>
-			<div class="worksBoxMain">
-				<h2 class="worksBoxMain_title">ロードサイン</h2>
-				<p class="worksBoxMain_text">ロードサイン（野立て看板）とは、道路沿線に設置される広告看板です。テレビやチラシ、雑誌といった広告ツールは瞬間的に大勢の人々に訴求することが出来ますが、多額な費用がかかるのと広告を継続しなければ効果は長続きしません。しかし、ロードサインは交通量がそのまま</p>
-				<div class="worksBoxMainMore">
-					<div class="button-more">
-						<a href="/works/road-sign/"><span>Read more</span></a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<section class="section">
-		<div class="worksBox is-second">
-			<div class="worksBoxMain">
-				<h2 class="worksBoxMain_title">一般製作</h2>
-				<p class="worksBoxMain_text">企業・店舗の顔となり、印象が大きく左右される大事なアイテムとなってきます。貴社の雰囲気や個性にマッチしたテイスト、ご要望に応じた各種異素材・様々な用途の看板を製作いたします。</p>
-				<div class="worksBoxMainMore">
-					<div class="button-more">
-						<a href="/works/production/"><span>Read more</span></a>
-					</div>
-				</div>
-			</div>
-			<div class="worksBox_img">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/works/thumbnail_2.jpg" alt="">
-			</div>
-		</div>
-	</section>
-	*/ ?>
 
 </main>
 
